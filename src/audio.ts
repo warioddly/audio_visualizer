6447b83189c19af828ce48afa18db5f91b7805e8
warioddly <53710487+warioddly@users.noreply.github.com>
2023-07-19 19:11:59 +0600

    add audio

--- src/audio.ts
+++ src/audio.ts
@@ -1,32 +1,58 @@
 
-// @ts-ignore
-import wavesAudio from './assets/audio/waves.mp3';
 
+
+const FFT_SIZE: number = 2048;
 
 export class AudioController {
+    public readonly audioElement: HTMLAudioElement;
+    public readonly analyser: AnalyserNode;
 
-    public audio: HTMLAudioElement;
+    private readonly bufferLength: number;
+    private readonly dataArray: Uint8Array;
 
     constructor(path: string) {
-
         const audioContext: AudioContext = new AudioContext();
-        const audioElement: HTMLAudioElement = new Audio(path);
-        const source: MediaElementAudioSourceNode = audioContext.createMediaElementSource(audioElement);
 
-        source.connect(audioContext.destination);
+        this.analyser = audioContext.createAnalyser();
+        this.analyser.fftSize = FFT_SIZE;
+
+        this.audioElement = new Audio(path);
+        const source: MediaElementAudioSourceNode = audioContext.createMediaElementSource(this.audioElement);
+
+        this.analyser.connect(audioContext.destination);
+        source.connect(this.analyser);
+
+        this.bufferLength = this.analyser.frequencyBinCount;
+        this.dataArray = new Uint8Array(this.bufferLength);
 
         window.addEventListener('click', () => {
+            if (this.audioElement.paused) {
+                this.audioElement.play();
 
-            console.log("play");
-
-            console.log(audioElement);
-
-            audioElement.play();
-
+                console.log("Playing");
+            } else {
+                this.audioElement.pause();
+                console.log("Paused");
+            }
         });
 
 
+        this.audioElement.addEventListener('play', () => {
+            this._getAudioData();
+        });
+
     }
 
+    private _getAudioData = () => {
+        this.analyser.getByteFrequencyData(this.dataArray);
 
+        for (let i = 0; i < this.bufferLength; i++) {
+            if (this.dataArray[i] !== 0) console.log(`Frequency bin ${i}: ${this.dataArray[i]}`);
+        }
+
+        setTimeout(() => {
+            this._getAudioData();
+        }, 1000 / (this.analyser.context.sampleRate / this.analyser.fftSize));
+
+    };
 }
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const stylesHandler = MiniCssExtractPlugin.loader;
 const config = {
     entry: './src/index.ts',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
     },
     devServer: {
