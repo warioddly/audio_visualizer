cc734f8636fbc0de022dbd9f4eb925174b099742
warioddly <53710487+warioddly@users.noreply.github.com>
2023-07-22 22:56:55 +0600

    add audio

--- src/audio.ts
+++ src/audio.ts
@@ -1,6 +1,8 @@
 
 // @ts-ignore
 import waves from './assets/audio/waves.mp3'
+// @ts-ignore
+import * as THREE from 'three';
 
 
 export class AudioController {
@@ -9,51 +11,87 @@ export class AudioController {
     public readonly analyser: AnalyserNode;
     public readonly dataArray: Uint8Array;
     private readonly bufferLength: number;
+    private readonly _audioContext: AudioContext;
 
     private _options: any = {
         fftSize: 2048,
         smoothingTimeConstant: 0.8,
         minDecibels: -90,
         maxDecibels: -10,
+        loop: false,
+        autoplay: false,
+        enableKeyboardControls: true,
+        windTimeConstant: 1,
     }
 
+
     constructor(path: string, options?: any) {
+
         waves;
 
-        const audioContext: AudioContext = new AudioContext();
-        this.analyser = audioContext.createAnalyser();
+        this.audioElement = new Audio(path);
+        this.audioElement.loop  = options.loop || this._options.loop;
+        this.audioElement.autoplay = options.autoplay || this._options.autoplay;
 
+        this._audioContext = new AudioContext();
+        const source: MediaElementAudioSourceNode = this._audioContext.createMediaElementSource(this.audioElement);
 
+        this.analyser = this._audioContext.createAnalyser();
         this.analyser.fftSize = options.fftSize || this._options.fftSize;
         this.analyser.smoothingTimeConstant = options.smoothingTimeConstant || this._options.smoothingTimeConstant;
         this.analyser.minDecibels = options.minDecibels || this._options.minDecibels;
         this.analyser.maxDecibels = options.maxDecibels || this._options.maxDecibels;
 
-
-        this.audioElement = new Audio(path);
-        const source: MediaElementAudioSourceNode = audioContext.createMediaElementSource(this.audioElement);
-
-        this.analyser.connect(audioContext.destination);
         source.connect(this.analyser);
-
+        this.analyser.connect(this._audioContext.destination);
+        
         this.bufferLength = this.analyser.frequencyBinCount;
         this.dataArray = new Uint8Array(this.bufferLength);
 
-        window.addEventListener('click', () => {
-            if (this.audioElement.paused) {
-                this.audioElement.play();
-                console.log("Playing");
-            } else {
-                this.audioElement.pause();
-                console.log("Paused");
-            }
-        });
+        console.log("enableKeyboardControls", options.enableKeyboardControls)
+
+        if (options.enableKeyboardControls ?? this._options.enableKeyboardControls) {
+            this._addKeyboardControls();
+        }
 
     }
+
 
     public getByteFrequencyData(): Uint8Array {
         this.analyser.getByteFrequencyData(this.dataArray);
         return this.dataArray;
+    }
+
+
+    public playOrPause(): void {
+        if (this.audioElement.paused) {
+            this.audioElement.play();
+            console.log("Playing");
+        } else {
+            this.audioElement.pause();
+            console.log("Paused");
+        }
+    }
+
+
+    private _addKeyboardControls(): void {
+
+        window.addEventListener('keydown', (event) => {
+
+            if (event.key === ' ') {
+                this.playOrPause();
+            }
+
+            if (event.key === 'ArrowRight') {
+                this.audioElement.currentTime += this._options.windTimeConstant;
+            }
+
+            if (event.key === 'ArrowLeft') {
+                this.audioElement.currentTime -= this._options.windTimeConstant;
+            }
+
+        });
+
     }
 
 
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@
 import * as THREE from 'three';
 // @ts-ignore
 import { AudioController } from './audio';
+// @ts-ignore
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 
 class Engine {
@@ -10,12 +12,14 @@ class Engine {
 
     private readonly _scene: THREE.Scene;
     private readonly _camera: THREE.PerspectiveCamera;
-    private _renderer: THREE.WebGLRenderer;
+    private readonly orbitControls: OrbitControls;
+    private readonly _renderer: THREE.WebGLRenderer;
+    private readonly _light: THREE.DirectionalLight;
 
     private _cube: THREE.Mesh;
+    private sphere: THREE.SphereGeometry;
 
-
-    private _audioController: AudioController;
+    private readonly _audioController: AudioController;
 
 
     constructor() {
@@ -24,6 +28,7 @@ class Engine {
         this._camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.01, 100 );
         this._camera.position.z = 5;
 
+
         this._renderer = new THREE.WebGLRenderer( { antialias: true } );
         this._renderer.setSize( window.innerWidth, window.innerHeight );
         this._renderer.setAnimationLoop( this._animation.bind(this) );
@@ -31,44 +36,78 @@ class Engine {
         this._renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( this._renderer.domElement );
 
+        this.orbitControls = new OrbitControls( this._camera, this._renderer.domElement );
+        this.orbitControls.enableDamping = true;
+        this.orbitControls.dampingFactor = 0.05;
+        this.orbitControls.screenSpacePanning = false;
+        this.orbitControls.maxDistance = 500;
+
+        this._light = new THREE.DirectionalLight( 0xffffff, 1 );
+        this._light.position.set( 1, 1, 1 ).normalize();
+        this._scene.add( this._light );
+        
+
 
         this._audioController = new AudioController('./assets/audio/waves.mp3', {
             smoothingTimeConstant: 0.7,
             minDecibels: -120,
             maxDecibels: -50,
+            loop: true,
+            autoplay: true,
         });
 
 
         const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
         const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
         this._cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        this._scene.add(this._cube);
+        // this._scene.add(this._cube);
 
+
+        const geometry = new THREE.SphereGeometry( 1, 32, 32 );
+        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); 
+        this.sphere = new THREE.Mesh( geometry, material );
+
+        this.sphere.position.x = 0;
+        this.sphere.position.y = 0;
+        this.sphere.position.z = 0;
+        
+        this._scene.add( this.sphere );
 
         window.addEventListener( 'resize', this._resize.bind(this) );
-
-
 
     }
 
 
     private _animation( time: any ) {
 
+    //    this._cubeAnimation();
+
+       this.orbitControls.update();
+
+       this._renderer.render( this._scene, this._camera );
+
+    }
+
+
+    private _cubeAnimation() {
+
         this._audioController.getByteFrequencyData();
 
         let dataArray = this._audioController.dataArray;
 
         const bassArray = dataArray.slice(0, dataArray.length / 4);
-
         const average = bassArray.reduce((acc, value) => acc + value, 0) / bassArray.length;
 
-        this._cube.scale.x = average / 100;
-        this._cube.scale.y = average / 100;
+        
 
-
-
-
-        this._renderer.render( this._scene, this._camera );
+        if (average < 100) {
+            this._cube.scale.x = 1;
+            this._cube.scale.y = 1;
+        }
+        else {
+            this._cube.scale.x = average / 100;
+            this._cube.scale.y = average / 100;
+        }
 
     }
 
